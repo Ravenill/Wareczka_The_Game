2b59db68f0000117c7d05aed6f379227d1295894
Rave <example@example.com>
2017-04-28 20:24:57 +0200

    #STOPER

--- Wareczka_The_Game/Gui.cpp
+++ Wareczka_The_Game/Gui.cpp
@@ -7,6 +7,21 @@
 
 void Gui::update(StatusCar dead)
 {
+	//TIME
+	sf::Time time = sf::milliseconds(60000) - clock.getElapsedTime();
+	if (time < sf::milliseconds(0)) time = sf::milliseconds(0);
+
+	int minutes, seconds, miliseconds;
+	minutes = time.asMilliseconds() / 60000;
+	time = time - sf::milliseconds(minutes * 60000);
+	seconds = time.asMilliseconds() / 1000;
+	time = time - sf::milliseconds(seconds * 1000);
+	miliseconds = time.asMilliseconds();
+
+	text_time[0].setString((minutes == 0) ? "00" : std::to_string(minutes));
+	text_time[2].setString(std::to_string(seconds));
+	text_time[4].setString(std::to_string(miliseconds));
+
 	std::string sc;
 	setDead(dead);
 
@@ -19,17 +34,20 @@ void Gui::update(StatusCar dead)
 void Gui::draw(sf::RenderTarget &target, sf::RenderStates states) const
 {
 	for (int i = 0; i < LP; i++) target.draw(text[i]);
+	for (int i = 0; i < 5; i++) target.draw(text_time[i]);
 }
 
 Gui::Gui()
 {
+	//Loading fonts
 	if (!font.loadFromFile("data/Neon.ttf"))
 	{
 		MessageBox(NULL, "Error! Font Streamster.tff not found. Please, download it and paste into /data folder.", ERROR, NULL);
 		return;
 	}
-	std::string content[] = { "Status: ", "Score: ", "0", "0"};
 
+	//GUI - SCORE, STATUS
+	std::string content[] = { "Status: ", "Score: ", "0", "0"};
 	for (int i = 0; i < LP; i++)
 	{
 		text[i].setFont(font);
@@ -42,6 +60,22 @@ Gui::Gui()
 	text[1].setPosition((RESX - 5 - text[1].getGlobalBounds().width), 35);
 	text[2].setPosition((5 + text[0].getGlobalBounds().width), (40 + text[0].getGlobalBounds().height));
 	text[3].setPosition((RESX - 5 - text[1].getGlobalBounds().width), (40 + text[1].getGlobalBounds().height));
+
+	//TIME
+	std::string content2[] = { "00", ":", "00", ":", "00" };
+	for (int i = 0; i < 5; i++)
+	{
+		text_time[i].setFont(font);
+		text_time[i].setString(content2[i]);
+		text_time[i].setCharacterSize(25);
+		text_time[i].setOrigin(text_time[i].getGlobalBounds().width / 2, text_time[i].getGlobalBounds().height / 2);
+	}
+
+	text_time[0].setPosition(((RESX / 2) - text_time[2].getGlobalBounds().width - text_time[3].getGlobalBounds().width) - 5, (RESY - 30 - text_time[0].getGlobalBounds().height - 30));
+	text_time[1].setPosition(((RESX / 2) - text_time[2].getGlobalBounds().width/2 - text_time[1].getGlobalBounds().width) - 1, (RESY - 30 - text_time[0].getGlobalBounds().height - 34));
+	text_time[2].setPosition(((RESX / 2)), (RESY - 30 - text_time[2].getGlobalBounds().height - 30));
+	text_time[3].setPosition(((RESX / 2) + text_time[2].getGlobalBounds().width/2 + text_time[3].getGlobalBounds().width) + 1, (RESY - 30 - text_time[4].getGlobalBounds().height - 34));
+	text_time[4].setPosition(((RESX / 2) + text_time[2].getGlobalBounds().width + text_time[3].getGlobalBounds().width) + 5, (RESY - 30 - text_time[4].getGlobalBounds().height - 30));
 
 	score = 0;
 	max_score = 0;
--- Wareczka_The_Game/Gui.h
+++ Wareczka_The_Game/Gui.h
@@ -9,9 +9,11 @@ class Gui : public sf::Drawable
 private:
 	sf::Font font;
 	sf::Text text[LP];
+	sf::Text text_time[5];
 	int score;
 	int max_score;
 	StatusCar is_dead;
+	sf::Clock clock;
 	
 	virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
 
